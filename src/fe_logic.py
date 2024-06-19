19ec5dae340fb872bf2fe6eb768174b298a757fc
Alicia <alicia.arenzana@gmail.com>
2024-06-19 11:14:29 +0200

    added folium

--- src/fe_logic.py
+++ src/fe_logic.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import folium
 from streamlit_folium import st_folium
 from src.network_analysis import NetworkExtractor, ElevationUpdater, NetworkAnalyzer
 from src.map_plotting import NetworkPlotter, CriticalPlotter
@@ -24,7 +25,7 @@ def load_network(city_name):
 def plot_network(network_with_elevation, city_name):
     st.markdown(f"### Road network of {city_name}", unsafe_allow_html=True)
     plotter = NetworkPlotter(network_with_elevation)
-    folium_map = plotter.plot_network_with_elevation()  
+    folium_map = plotter.plot_network_with_elevation()
     st_folium(folium_map, width=725, height=500)
 
 # Analyze the network
@@ -38,6 +39,6 @@ def plot_network_critical(analyzer):
     st.markdown("### Road network analysis", unsafe_allow_html=True)
     critical_plotter = CriticalPlotter(analyzer.G, analyzer.nodes_removed, analyzer.critical_elevation)
     folium_map = critical_plotter.prepare_and_plot()
-    
+
     if folium_map:
         st_folium(folium_map, width=725, height=500)
